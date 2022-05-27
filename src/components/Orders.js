42f981a492b40c3255a815b822fd42da8f6a18a9
Alireza Talaei <alireza@Alirezas-MacBook-Pro.local>
2022-05-27 12:43:26 +1000

    checkout page created

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,30 +1,77 @@
-import React, {useEffect, useState} from "react";
+import React, {Component, useEffect, useState} from "react";
 import axios from "axios";
 
 const SERVER_URL = 'http://localhost:3000/orders.json'
+const SERVER_URL_CUSTOMER = 'http://localhost:3000/customers.json'
 
 const Orders = (props) => {
-    const [orders, setOrders] = useState([]);
-    const current_order = props.orderID 
-    console.log(props.orderID);  
-        useEffect (() => {
-            axios(SERVER_URL, {}).then((response) => {                
-                setOrders(response.data);                
-                console.log(orders);
-            });
-        }, []);
-        // console.log(orders);
-        // orders.forEach(function() {
-        //     if (orders.id === current_order){
-        //         console.log('found');
-        //     }
-        // })     
+    const [order, setOrder] = useState([]);
+    const current_order = props.orderID;
         
+       
+        useEffect(() => {
+            axios(`http://localhost:3000/orders/${current_order}.json`).then((response) => {
+                setOrder(response.data);
+                console.log(order)
+            })
+        }, []);
+
+               
     return(
         <div>
-            order coming soon
+            <p>{order.product_ids}</p>
+            {order.customer_id == null ? <CustomerForm current_order={current_order}/> : <FinaliseOrder />}
         </div>
     )
 }
 
+const CustomerForm = (props) => {
+    const current_order = props.current_order
+   const [name, setName] = useState('');
+   const [phone, setPhone] = useState('');
+   const [customer_id, setCustomerID] = useState('');
+   console.log(current_order);
+
+    const _handleChangeName = (event) => {
+        setName(event.target.value);              
+    }
+
+    const _handleChangePhone = (event) => {
+        setPhone(event.target.value);
+    }
+
+    const _handleSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post(SERVER_URL_CUSTOMER, {customer:{name: name, phone: phone}}).then((response) => {
+            setCustomerID(response.data.id);
+        })
+        await axios.put(`http://localhost:3000/orders/${current_order}.json`, {order:{customer_id: customer_id}}).then((response) => {
+            console.log(response.data);
+        })
+        
+    }
+    return(
+        <form onSubmit={_handleSubmit}>
+            <label>Your Name:
+                <input type='text' onChange={_handleChangeName} value={name} />
+            </label>
+
+            <label>Your Phone Number:
+                <input type='text' onChange={_handleChangePhone} value={phone} />
+            </label>
+            
+            <input type='submit' value='Enter your details' />
+        </form>
+    )
+    
+}
+
+const FinaliseOrder = (props) => {
+    
+    return(
+        <div>
+            <button>Pay</button>
+        </div>
+    )
+}
 export default Orders;
